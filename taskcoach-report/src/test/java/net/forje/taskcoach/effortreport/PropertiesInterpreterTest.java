feebeeb514a7f484447716e09c7f76d337456e1f
3URNINGCHR0M3 <3urningchr0m3@gmail.com>
2013-01-31 10:11:43 -0500

    add test for new class

--- taskcoach-report/src/test/java/net/forje/taskcoach/effortreport/PropertiesInterpreterTest.java
+++ taskcoach-report/src/test/java/net/forje/taskcoach/effortreport/PropertiesInterpreterTest.java
@@ -1,4 +1,53 @@
 package net.forje.taskcoach.effortreport;
 
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.util.Properties;
+
 public class PropertiesInterpreterTest {
+
+    @Test
+    public void testIsLevelThree() throws Exception {
+
+        final Properties properties = new Properties();
+        properties.setProperty(AutoHotKeyEffortReport.PHASE, "E.APLEVEL3");
+
+        final PropertiesInterpreter interpreter = new PropertiesInterpreter(properties);
+        Assert.assertEquals(true, interpreter.isLevelThree());
+
+    }
+
+    @Test
+    public void testIsReleaseRelated() throws Exception {
+
+        final Properties properties = new Properties();
+        properties.setProperty(AutoHotKeyEffortReport.CLIENT, "807424");
+
+        final PropertiesInterpreter interpreter = new PropertiesInterpreter(properties);
+
+        Assert.assertEquals(true, interpreter.isReleaseRelated());
+
+    }
+
+    @Test
+    public void testGetRelease() throws Exception {
+        final Properties properties = new Properties();
+        properties.setProperty(PropertyNames.Release.toString(), "4.0");
+
+        final PropertiesInterpreter interpreter = new PropertiesInterpreter(properties);
+        Assert.assertEquals("4.0", interpreter.getRelease());
+    }
+
+    @Test
+    public void testGetJiraKey() throws Exception {
+
+        final Properties properties = new Properties();
+        properties.setProperty(PropertyNames.JiraKey.toString(), "ERX-99999");
+
+        final PropertiesInterpreter interpreter = new PropertiesInterpreter(properties);
+        Assert.assertEquals("ERX-99999", interpreter.getJIRAKey());
+
+    }
+
 }
