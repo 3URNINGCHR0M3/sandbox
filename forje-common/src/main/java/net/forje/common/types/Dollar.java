2a62cee9d403c2a5f440c89024f32b74416bfbcd
Brian Surratt <Brian.Surratt@dcsg.com>
2016-10-30 18:37:23 -0400

    continuing development of Dollar class

---
+++ forje-common/src/main/java/net/forje/common/types/Dollar.java
@@ -0,0 +1,126 @@
+package net.forje.common.types;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/**
+ *
+ */
+public class Dollar implements Comparable<Dollar> {
+
+    public static Dollar ZERO = new Dollar(0);
+    public static Dollar ONE = new Dollar(100);
+
+    private static final int DEFAULT_SCALE = 2;
+    public static BigDecimal ONE_HUNDRED = new BigDecimal("100.00").setScale(DEFAULT_SCALE);
+
+    private String _toString;
+
+    private static RoundingMode defaultRoundingMode = RoundingMode.HALF_UP;
+
+    private int _cents;
+
+    private Dollar(final int cents) {
+        _cents = cents;
+    }
+
+    public static void setDefaultRoundingMode(final RoundingMode defaultRoundingMode) {
+        Dollar.defaultRoundingMode = defaultRoundingMode;
+    }
+
+    public static RoundingMode getDefaultRoundingMode() {
+        return defaultRoundingMode;
+    }
+
+    public Dollar add(final Dollar toAdd) {
+
+        final int sum = this._cents + toAdd._cents;
+        final Dollar dollar = new Dollar(sum);
+
+        return dollar;
+
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final Dollar dollar = (Dollar) o;
+
+        return _cents == dollar._cents;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return _cents;
+    }
+
+    @Override
+    public int compareTo(final Dollar toCompare) {
+
+        return this._cents - toCompare._cents;
+
+    }
+
+
+    public BigDecimal bigDecimalValue() {
+
+        final BigDecimal value = new BigDecimal(_cents / 100);
+        return value.setScale(DEFAULT_SCALE);
+
+    }
+
+    public static Dollar valueOf(final String value) {
+        BigDecimal working = new BigDecimal(value).setScale(DEFAULT_SCALE);
+        working = working.multiply(new BigDecimal("100"));
+        final int cents  = working.intValue();
+
+        return new Dollar(cents);
+
+    }
+
+    public Dollar subtract(final Dollar toSubtract) {
+
+        final int result = this._cents - toSubtract._cents;
+
+        return new Dollar(result);
+
+    }
+
+    public static Dollar valueOf(final BigDecimal input) {
+
+       return valueOf(input, defaultRoundingMode);
+
+    }
+
+    public static Dollar valueOf(final BigDecimal input,
+                                 final RoundingMode roundingMode) {
+
+        final BigDecimal scaled = input.setScale(DEFAULT_SCALE, roundingMode);
+        final BigDecimal centsBD = scaled.multiply(ONE_HUNDRED);
+        final int cents = centsBD.intValue();
+
+        return new Dollar(cents);
+
+    }
+
+
+    @Override
+    public String toString() {
+
+        if (_toString == null) {
+            final BigDecimal bigDecimal = new BigDecimal(_cents);
+            final BigDecimal result = bigDecimal.divide(ONE_HUNDRED);
+            _toString = result.toString();
+        }
+
+        return _toString;
+
+    }
+
+
+
+}
---
+++ forje-common/src/test/java/net/forje/common/types/DollarTest.java
@@ -0,0 +1,249 @@
+package net.forje.common.types;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import static org.junit.Assert.fail;
+
+/**
+ *
+ */
+public class DollarTest {
+
+    private RoundingMode _roundingMode;
+
+    @Test
+    public void testImplementsComparable() throws Exception {
+        Assert.assertEquals(true, Comparable.class.isAssignableFrom(Dollar.class));
+    }
+
+    @Test
+    public void testEqualsTrue() throws Exception {
+        Assert.assertEquals(true, Dollar.ONE.equals(Dollar.ONE));
+    }
+
+
+    @Test
+    public void testEqualsFalse() throws Exception {
+        Assert.assertEquals(false, Dollar.ZERO.equals(Dollar.ONE));
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+        Dollar sum = Dollar.ZERO.add(Dollar.ONE);
+        Assert.assertEquals(Dollar.ONE, sum);
+    }
+
+    @Test
+    public void testSubtract() throws Exception {
+
+        final Dollar alpha = Dollar.valueOf("9.99");
+        final Dollar beta = Dollar.valueOf("3.33");
+
+        final Dollar expected = Dollar.valueOf("6.66");
+
+        Assert.assertEquals(expected, alpha.subtract(beta));
+
+    }
+
+    @Test
+    public void testSubtractNegativeResult() throws Exception {
+
+        final Dollar alpha = Dollar.valueOf("9.99");
+        final Dollar beta = Dollar.valueOf("3.33");
+
+        final Dollar expected = Dollar.valueOf("-6.66");
+
+        Assert.assertEquals(expected, beta.subtract(alpha));
+
+    }
+
+    @Test
+    public void testCompareLessThan() throws Exception {
+        Assert.assertEquals(true, Dollar.ZERO.compareTo(Dollar.ONE) < 0);
+    }
+
+    @Test
+    public void testCompareGreaterThan() throws Exception {
+        Assert.assertEquals(true, Dollar.ONE.compareTo(Dollar.ZERO) > 0);
+    }
+
+    @Test
+    public void testCompareEquals() throws Exception {
+        Assert.assertEquals(0, Dollar.ZERO.compareTo(Dollar.ZERO));
+    }
+
+    @Test
+    public void testZeroConstant() throws Exception {
+        final Dollar zero = Dollar.valueOf("0.00");
+        Assert.assertEquals(zero, Dollar.ZERO);
+    }
+
+    @Test
+    public void testOneConstant() throws Exception {
+        final Dollar zero = Dollar.valueOf("1.00");
+        Assert.assertEquals(zero, Dollar.ONE);
+    }
+
+    @Test
+    public void testBigDecimalValueScale2() throws Exception {
+        Assert.assertEquals(2, Dollar.ONE.bigDecimalValue().scale());
+    }
+
+    @Test
+    public void testAddImmutable() throws Exception {
+
+        final Dollar first = Dollar.valueOf("9.99");
+        final Dollar reference = Dollar.valueOf("9.99");
+        final Dollar second = Dollar.valueOf("0.01");
+
+        Assert.assertNotSame(first, reference);
+        Assert.assertEquals(first, reference);
+
+        final Dollar sum = first.add(second);
+
+        Assert.assertEquals(first, reference);
+
+        Assert.assertNotSame(sum, first);
+
+    }
+
+    @Test
+    public void testSubtractImmutable() throws Exception {
+
+        final Dollar alpha = Dollar.valueOf("119.33");
+        final Dollar beta = Dollar.valueOf("2.33");
+
+        final Dollar result = alpha.subtract(beta);
+
+        Assert.assertEquals(Dollar.valueOf("119.33"), alpha);
+        Assert.assertEquals(Dollar.valueOf("2.33"), beta);
+
+    }
+
+    @Test
+    public void testMultiply() throws Exception {
+        fail("test not implemented");
+    }
+
+    @Test
+    public void testMultiplyImmutable() throws Exception {
+        fail("test not implemented");
+    }
+
+    @Test
+    public void testDivide() throws Exception {
+        fail("test not implemented");
+        // will return the largest quantity divisible by the provided value.
+        // divide 10.00 by 3, get 3.33
+    }
+
+    @Test
+    public void testIndivisible() throws Exception {
+        Assert.fail("test not implemented");
+        // divide 0.02 by 3
+        // not sure what it should do
+
+    }
+
+    @Test
+    public void testValueOfBigDecimal() throws Exception {
+        final String stringValue = "99.95";
+        final Dollar actual = Dollar.valueOf(new BigDecimal(stringValue));
+        final Dollar expected = Dollar.valueOf(stringValue);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testValueOfBigDecimalUsesRoundingMode() throws Exception {
+
+
+
+        // this has too many decimal points
+        final BigDecimal value = new BigDecimal("9.115");
+
+        Dollar.setDefaultRoundingMode(RoundingMode.HALF_UP);
+        Dollar halfUp = Dollar.valueOf(value);
+        Assert.assertEquals(Dollar.valueOf("9.12"), halfUp);
+
+        Dollar.setDefaultRoundingMode(RoundingMode.HALF_DOWN);
+        Dollar halfDown = Dollar.valueOf(value);
+        Assert.assertEquals(Dollar.valueOf("9.11"), halfDown);
+
+
+
+    }
+
+    @Before
+    public void beforeTest() {
+        _roundingMode = Dollar.getDefaultRoundingMode();
+    }
+
+    @After
+    public void afterTest() {
+        Dollar.setDefaultRoundingMode(_roundingMode);
+    }
+
+    @Test
+    public void testValueOfBigDecimalWithRoundingMode() throws Exception {
+
+        Dollar.setDefaultRoundingMode(RoundingMode.HALF_UP);
+        final BigDecimal input = new BigDecimal("3.335");
+        final Dollar value = Dollar.valueOf(input, RoundingMode.HALF_DOWN);
+        Assert.assertEquals(Dollar.valueOf("3.33"), value);
+
+    }
+
+    @Test
+    public void testValueOfStringWholeDollar() throws Exception {
+
+        Dollar result = Dollar.valueOf("1");
+        Assert.assertEquals(Dollar.ONE, result);
+
+    }
+
+    @Test
+    public void testValueOfStringFractionalDollar() throws Exception {
+
+        Dollar oneDollars = Dollar.valueOf("1");
+        Dollar nintyNineCents = Dollar.valueOf("0.99");
+        Dollar nintyEightCents = Dollar.valueOf("0.98");
+        Assert.assertEquals(true, nintyNineCents.compareTo(oneDollars) < 0);
+        Assert.assertEquals(true, nintyNineCents.compareTo(nintyEightCents) > 0);
+
+    }
+
+    @Test
+    public void testHashCodeEqualsTrue() throws Exception {
+        Assert.assertEquals(true, Dollar.valueOf("19.95").hashCode() == Dollar.valueOf("19.95").hashCode());
+    }
+
+    @Test
+    public void testHashCodeEqualsFalse() throws Exception {
+        Assert.assertEquals(true, Dollar.valueOf("12.34").hashCode() != Dollar.valueOf("23.45").hashCode());
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+        final String stringValue = "8.85";
+        final Dollar test = Dollar.valueOf(stringValue);
+        Assert.assertEquals(stringValue, test.toString());
+
+    }
+
+    @Test
+    public void testToStringNegative() throws Exception {
+
+        final String stringValue = "-8.85";
+        final Dollar test = Dollar.valueOf(stringValue);
+        Assert.assertEquals(stringValue, test.toString());
+
+    }
+
+}
