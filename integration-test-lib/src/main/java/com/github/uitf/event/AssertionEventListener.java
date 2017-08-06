cbe68a1ca9c8226edf8b78b7c4f2b83901b4dc11
Brian Surratt <Brian.Surratt@dcsg.com>
2017-08-05 21:33:00 -0400

    added methods for AssertionEventListener

--- integration-test-lib/src/main/java/com/github/uitf/event/AssertionEventListener.java
+++ integration-test-lib/src/main/java/com/github/uitf/event/AssertionEventListener.java
@@ -6,4 +6,20 @@ import java.util.EventListener;
  *
  */
 public interface AssertionEventListener extends EventListener {
+
+
+    /**
+     * Called when an assertion is executed by a test
+     *
+     * @param event details of the assertion
+     */
+    public void assertion(AssertEvent event);
+
+    /**
+     * Called when an failure is produced by a test
+     *
+     * @param event details of the failure
+     */
+    public void failure(AssertEvent event);
+
 }
